54c9f4ea52ee985386088e2f735b4dd609056468
RoJaebl <qgs0924@gmail.com>
2022-09-02 22:07:23 +0900

    #1.2 Block scope

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 /**
- * 작성한 소스코드
+ * scope 안에서 선언된 var 변수는
+ * scope 밖에 있는 console이 hello변수에 접근이 가능하다.
  */
-console.log(name);
-// < undefined
-var name = "JavaScript";
+if (true) {
+  var hello1 = "hello1";
+}
+console.log(hello1);
 
 /**
- * 인터프리터된 소스코드
- * hoisting으로인해 아래와 같은 코드로 변역되어 실행이 되는것이다.
- * 결국 console은 데이터가 없고 선언만 된 변수를 출력하는 결과를 낳는다.
+ * scope 안에서 선언된 let, const 변수는
+ * scope 밖에 있는 console이 hello변수에 접근이 불가능하다.
  */
-var name;
-conseloe.log(name);
-// < undefined
-name = "JavaScript";
+if (true) {
+  let hello2 = "hello2";
+}
+console.log(hello2);
+
+/**
+ * function scope에서 var은 let과 const와 규칙이 동일하다.
+ */
+function hello() {
+  var hello3 = "hello3";
+}
+console.log(hello3);

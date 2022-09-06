0c3c8275c66a7557942c3bde9ab2c4f7e83c0433
RoJaebl <qgs0924@gmail.com>
2022-09-06 21:07:10 +0900

    #3.0 Template literal 'backtick'

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 /**
- * 파라미터에 'nico'를 주어 함수를 호출할때 아무런 인수를 넣지 않을때
- * 지정한 default value로 함수를 동작하게 할 수 있다.
- * @param {string} aName string으로 인수를 받는다.
- * @returns aNmae으로 받은 값을 'Hello'와 더해서 반환된다.
+ * @description `"Hello " + aName`이걸 template literals라고 부른다.
+ * string form 앞뒤로 백틱``을 붙혀 쓰여진 모든 문자를 string으로 변환해준다.
  */
-function sayHi1(aName = "nico") {
-  return "Hello " + aName;
-}
+const sayHi1 = (aName = "anon") => `Hello ${aName} lovely to have you`;
+// `"Hello " + aName + " lovely to have you"`;
 console.log(sayHi1());
 
 /**
- * arrow function으로 default value를 사용하는 모습이다.
- * @param {string} aName string으로 인수를 받는다.
- * @returns aNmae으로 받은 값을 'Hello'와 더해서 반환된다.
+ * @description ${}안에 함수를 넣어 사용할 수 있다.
  */
-const sayHi2 = (aName = "anon") => "Hello " + aName;
+const sum = (a, b) => a + b;
+const sayHi2 = (aName = "anon") => `Hello ${sum(1, 2)} lovely to have you`;
+// `"Hello " + aName + " lovely to have you"`;
 console.log(sayHi2());

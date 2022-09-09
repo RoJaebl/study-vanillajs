0d6bac94759eddfbe8afd0c489dfbb662f234d28
RoJaebl <qgs0924@gmail.com>
2022-09-09 15:26:03 +0900

    #8.0 Introduction to Async

--- src/index.js
+++ src/index.js
@@ -1,26 +1,9 @@
-const friends = ["nico", "lynn", "dar", "mark"];
-
-for (let i = 0; i < 20; i++) {
-  console.log("I love `Kimchi`");
-}
-
-for (let i = 0; i < friends.length; i++) {
-  console.log(`${friends[i]} love \`Kimchi\``);
-}
-
-const addHeart = (...args) => console.log(args);
-friends.forEach(addHeart);
-
 /**
- * for of는 forEach와 다르게 string, array, NodeLists,
- * typed array, map, set에서 사용할 수 있다. forEach는
- * array에서만 사용이 가능하다. 또한 break 키워드를 사용하여
- * `원하는 위치까지 반복문을 실행`하고 빠져 나올 수 있다.
+ * 이론상 fetch에 에러가 발생하고 console이 실행 되어야
+ * 하지만, 그렇지 않고 console이 먼저 실행이 되고 fetch의
+ * 에러상태가 출력된다.
  */
-for (const friend of friends) {
-  if (friend === "dar") {
-    break;
-  } else {
-    console.log(friend);
-  }
-}
+const hello = fetch("https://google.com");
+
+console.log("something");
+console.log(hello);

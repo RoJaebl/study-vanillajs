d4d3fa30106a7fb8376debc3a17ed0c5812c5fbd
RoJaebl <qgs0924@gmail.com>
2022-09-09 11:10:56 +0900

    #6.0 Introduction to Spread

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 /**
- * array destructuring으로 손쉽게 변수를 서로 교환할 수 있다.
- * swap하고자하는 변수를 array를 만들고 destructuring을 이용하여
- * 값을 서로 바꾸는 것이다.
- * @decription
- * [sat, mon] = [mon, sat] => [sat, mon] = ["Sat", "Mon"]
+ * `spread ...`를 사용하여 array나 object를 해채하여 안의
+ * 모든 요소들을 반환한다.
  */
-let mon = "Sat";
-let sat = "Mon";
+const friends = [1, 2, 3, 4];
+const family = ["a", "b", "c"];
+console.log([...family, ...friends]);
 
-[sat, mon] = [mon, sat];
+const sexy = {
+  name: "nico",
+  age: 24,
+};
 
-/**
- * array destructuring으로 array의 어느 중간 부분을 가지고 오고
- * 싶다면 array destructuring에 `콤마,`를 체워 원하는 위치를
- * 변수로 선언한다.
- */
-const days = ["mon", "tue", "wed", "thu", "fir", "sat", "sun"];
+const hello = {
+  sexy: true,
+  hello: "hello",
+};
 
-const [, , , thu, fir] = days;
-console.log(thu, fir);
+console.log({ ...sexy, ...hello });

25abacf4c5dcb089a99b81f29e89404fcb92b3f8
RoJaebl <qgs0924@gmail.com>
2022-09-11 13:23:29 +0900

    #8.3 Chaining Promises

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 /**
- * then으로 전달된 인수는 promise에 처리한 resolve와 recject의
- * 값을 전달 받는다.
+ * Promise를 chain으로 묶어서 연속된 함수를 비동기적으로 실행시킬
+ * 수 있다. 또한 chain된 then 중간에 에러가 발생하여도 catch를
+ * 정의하여 사용할 수도 있다.
  */
-const amISexy1 = new Promise((resolve, reject) => {
-  resolve(`Yes you are!`);
+const amISexy = new Promise((resolve, reject) => {
+  resolve(2);
 });
 
-amISexy1.then((value) => console.log(value));
+const timesTwo = (number) => number * 2;
 
-/**
- * reject만 쓰이면 JS에서 에러를 반환한다. 그렇기에 에러를
- * catch하여 에러에 관한 처리소스를 구현해야한다. 여기서 then과
- * catch가 함수로 보여서 then실행 후 cath가 실행되는거라 생각
- * 되지만, 실상 두 함수는 서로 각각 동작을 한다.
- * JS는 다른작업이 딜레이걸리면 다른 작업을 실행하는 시스템
- * 이기에 함수 디자인을 JS시스템을 고려하여 구현해야한다.
- */
-const amISexy2 = new Promise((resolve, reject) => {
-  setTimeout(reject, 3000, `Yes are ugly`);
-});
-
-amISexy2.then((result) => console.log(result)).catch((err) => console.log(err));
+amISexy
+  .then(timesTwo)
+  .then(timesTwo)
+  .then(timesTwo)
+  .then(() => {
+    throw Error(`Something is wrong`);
+  })
+  .then(timesTwo)
+  .then(timesTwo)
+  .then((lastNumber) => console.log(lastNumber))
+  .catch((err) => console.log(err));

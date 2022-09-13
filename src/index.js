3ad9da5e43726c66e427a1a9b1c22f812631a664
RoJaebl <qgs0924@gmail.com>
2022-09-13 22:56:20 +0900

    #9.0 Async Await

--- src/index.js
+++ src/index.js
@@ -1,27 +1,23 @@
 /**
- * race은 array로 제공된 작업을 병렬로 실행시켜주는 함수이다.
- * array를 동시에 작업하여 에러가 발생하면 진행하던 다른
- * 작업들도 중단하고 예외를 발생시킨다. 예외없이 작업이 진행이
- * 된다면 빠르게 끝나는 작업의 값을 반환한다.
+ * 지금까지 보았던 promise를 async로 변경하여 사용하는 것을
+ * 비교하면 몰라보게 가독성이 더욱 좋아지고 무엇보다 비동기적
+ * 소스를 동기적 코드와 같이 절차적(await)으로 실행할 수
+ * 있게 구현할 수 있다.
  */
-const p1 = new Promise((resolve) => {
-  setTimeout(resolve, 5000, "First");
-});
+const getMoviesPromise = () => {
+  fetch("https://yts.mx/api/v2/list_movies.json")
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
+    .then((json) => console.log(json))
+    .catch((err) => console.log(`❌ ${err}`));
+};
 
-/*
-const p2 = new Promise((resolve) => {
-  setTimeout(resolve, 1000, "Second");
-});
-*/
-const p2 = new Promise((resolve, reject) => {
-  setTimeout(reject, 5000, "I hate JS");
-});
-const p3 = new Promise((resolve) => {
-  setTimeout(resolve, 3000, "Third");
-});
+const getMoviesAsync = async () => {
+  const response = await fetch("https://yts.mx/api/v2/list_movies.json");
+  const json = await response.json();
+  console.log(json);
+};
 
-const motherPromise = Promise.race([p1, p2, p3]);
-
-motherPromise
-  .then((values) => console.log(values))
-  .catch((err) => console.log(err));
+getMoviesAsync();

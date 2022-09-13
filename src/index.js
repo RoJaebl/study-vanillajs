3aeb79c9117b969f89c16b6caa6f66cfcc814d9c
RoJaebl <qgs0924@gmail.com>
2022-09-13 23:10:12 +0900

    #9.1 try catch finally

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 /**
- * 지금까지 보았던 promise를 async로 변경하여 사용하는 것을
- * 비교하면 몰라보게 가독성이 더욱 좋아지고 무엇보다 비동기적
- * 소스를 동기적 코드와 같이 절차적(await)으로 실행할 수
- * 있게 구현할 수 있다.
+ * async/await말고도 promise와 함께 사용했었던 catch와
+ * finally를 아래와 같이 사용할 수 있다. catch는 try구문
+ * 안에 들어가 있는 모든 소스에서 에러가 발생했을때 발생한
+ * 에러 인자를 catch로 전달하여 예외를 처리할 수 있게 한다.
+ * 또한 try와 catch다음으로 동작하는 finally로 미자막으로
+ * 꼭 실행해야하는 소스코드를 추가함으로서 promise가 사용
+ * 했었던 모든 기능을 구현할 수 있다.
  */
-const getMoviesPromise = () => {
-  fetch("https://yts.mx/api/v2/list_movies.json")
-    .then((response) => {
-      console.log(response);
-      return response.json();
-    })
-    .then((json) => console.log(json))
-    .catch((err) => console.log(`❌ ${err}`));
-};
-
 const getMoviesAsync = async () => {
-  const response = await fetch("https://yts.mx/api/v2/list_movies.json");
-  const json = await response.json();
-  console.log(json);
+  try {
+    const response = await fetch("https://yts.mx/api/v2/list_movies.json");
+    const json = await response.json();
+    throw Error("Im hungry");
+  } catch (err) {
+    console.log(`❌ ${err}`);
+  } finally {
+    console.log("We are done!");
+  }
 };
 
 getMoviesAsync();

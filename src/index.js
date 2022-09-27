d202b76b15e8a2f8d5c56c8cbf8c442a8012b611
RoJaebl <qgs0924@gmail.com>
2022-09-27 22:54:09 +0900

    #12.1 Proxies

--- src/index.js
+++ src/index.js
@@ -1,28 +1,38 @@
 /**
- * Generators를 사용하는 방법은 function뒤에 `*`을 달아주면 된다.
- * `yield`로 return과 비슷한 작업을 수행 할 수 있다.
+ * Proxy는 filter처럼 생각할 수 있다. Proxy객체는 object와 그 object를
+ * 제어하는 헨들러 인수를 받는다. 그리고 아래 처럼 핸들러 역할을 할 object를
+ * 비워두고 `filteredUser`변수를 object propety를 호출하는거첨 접근하면
+ * object값을 받아 올 수 있다.
  */
-function* listPeople() {
-  yield "Dal";
-  yield "Flynn";
-  yield "Mark";
-  yield "Godkimchi";
-  yield "Japan Guy";
-}
+const userObj = {
+  username: "nico",
+  age: 12,
+  password: 1234,
+};
+const userFilter1 = {};
+const filteredUser1 = new Proxy(userObj, userFilter1);
 
 /**
- * Generators를 호출하면 yield와 관련된 데이터가 출력되지 않는데
- * `.next()`를 호출하여 yield순서대로 지정한 데이터에 접근할 수 있다.
+ * 하지만 헨들러 역할을 하는 object에 get과 set을 정의하여 변수에 요소접근을
+ * 시도하면 get이 호출이되고 변수에 요소를 새롭게 정의하면 set이 호출이된다.
+ * 물론 새로 정의한 요소도 추가되지 않는걸 확인할 수 있다.
+ * 기본적으로 proxy객체의 요소를 가지고 오는 방법은 get에 다음과 같이 정의하여
+ * 가지고 오는 방법을 한다. `return target[prop];` 또한 삼항 연산자나 다른
+ * 정의로 요소를 가공하여 출력할 수도 있고 데이터를 변형시킬 수 있다.
  */
-const listG = listPeople();
-
-/**
- * yield를 반복문안에 정의하여 다양한 방법으로 yield를 호출할 수 있다.
- */
-const friends = ["Dal", "Flynn", "Mark", "Godkimchi", "Japan Guy"];
-function* friendTeller() {
-  for (const friend of friends) {
-    yield friend;
-  }
-}
-const friendLooper = friendTeller();
+const userFilter2 = {
+  get: (target, prop, receiver) => {
+    return prop == "password" ? `*`.repeat(5) : target[prop];
+  },
+  set: () => {
+    console.log(`Somebody wrote something`);
+  },
+  deleteProperty: (target, prop) => {
+    if (prop == "password") {
+      return;
+    } else {
+      target[prop] = "DELETE";
+    }
+  },
+};
+const filteredUser2 = new Proxy(userObj, userFilter2);
